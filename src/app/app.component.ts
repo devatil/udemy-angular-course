a8f2d26f84a6c8d5c8b24bee3f965803198338db
devatil <ngregister@outlook.com>
2020-08-05 03:57:06 +0300

    adding bootstrap and ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'udemy-angular-course';
+export class AppComponent implements OnInit {
+	title = 'udemy-angular-course';
+
+	constructor(private toastr: ToastrService, private spinner: NgxSpinnerService) {}
+
+	ngOnInit() {
+		this.showSuccess();
+		this.showSpinner();
+	}
+
+	showSuccess() {
+		this.toastr.success('Angular Udemy Egitimine Hosgeldiniz', 'Giris Basarili');
+	}
+
+	showSpinner() {
+		this.spinner.show();
+
+		setTimeout(() => {
+			/** spinner ends after 5 seconds */
+			this.spinner.hide();
+		}, 5000);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      progressBar: true,
+      easing: 'ease-in',
+      closeButton: false,
+      progressAnimation: 'decreasing',
+      preventDuplicates: true,
+      positionClass: 'toast-bottom-left'
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+}
