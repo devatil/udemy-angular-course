947d9daf6cffd07fb7bb66142073896f8bacc43b
devatil <ngregister@outlook.com>
2020-08-13 18:26:58 +0300

    input and output added

--- src/app/parent1/parent1/parent1.component.ts
+++ src/app/parent1/parent1/parent1.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Child1Component } from '../../../libs';
 
 @Component({
   selector: 'app-parent1',
@@ -6,8 +7,30 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./parent1.component.scss'],
 })
 export class Parent1Component implements OnInit {
+  @ViewChild(Child1Component) child1: Child1Component;
+
   name = '';
+  calculateData;
+  succesStatus = 0;
   constructor() {}
 
   ngOnInit(): void {}
+
+  calculate() {
+    this.calculateData = {
+      name: this.name,
+      status: this.succesStatus,
+    };
+  }
+
+  resetFormFunc(name) {
+    alert(name);
+    this.name = '';
+    this.succesStatus = 0;
+    this.calculateData = '';
+  }
+
+  childTrigger() {
+    this.child1.alertChl();
+  }
 }
--- src/libs/child-component/child1/child1.component.ts
+++ src/libs/child-component/child1/child1.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child1',
@@ -7,8 +7,23 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class Child1Component implements OnInit {
   @Input()
-  nameVal: string;
+  data: any;
+  @Output()
+  resetForm: EventEmitter<any> = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {}
+
+  emitEvent(name) {
+    this.resetForm.emit(name);
+  }
+
+  alertChl() {
+    alert('child componentine ulastiniz..');
+    console.log('Child componentini tetiklediniz..');
+    this.data = {
+      name: 'triggered',
+      succesStatus: 'false',
+    };
+  }
 }
