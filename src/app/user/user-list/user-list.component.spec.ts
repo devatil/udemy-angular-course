4133174261887fb5cd6876ee2a243ba2b76c1f6e
devatil <ngregister@outlook.com>
2020-08-24 09:52:28 +0300

    add upload

--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -1,6 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UserListComponent } from './user-list.component';
+import {UserListComponent} from './user-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import {By} from '@angular/platform-browser';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
@@ -8,9 +10,9 @@ describe('UserListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserListComponent ]
-    })
-    .compileComponents();
+      declarations: [UserListComponent],
+      imports: [HttpClientModule],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +23,34 @@ describe('UserListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should render h1 tag', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Kullanici ekle'
+    );
+  });
+  it('should be an honour h3 tag ile alakali', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h3').textContent).toContain(
+      'Kullanici Listesi Bos'
+    );
+  });
+  it('should be increase user list items', () => {
+    const button = fixture.debugElement.query(By.css('.add-button'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    const listItem = fixture.debugElement.queryAll(By.css('.user-list-item'));
+    expect(listItem.length).toEqual(1);
+  });
+  it('should be increase user list items 3 times', () => {
+    const button = fixture.debugElement.query(By.css('.add-button'));
+
+    for (let kp = 0; kp < 3; kp++) {
+      button.triggerEventHandler('click', null);
+    }
+    fixture.detectChanges();
+    const listItem = fixture.debugElement.queryAll(By.css('.user-list-item'));
+    expect(listItem.length).toEqual(3);
   });
 });
--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -8,6 +8,37 @@ import { UserService } from '../../../libs';
 })
 export class UserListComponent implements OnInit {
   userData;
+  users = [];
+  randomUser = [
+    {
+      id: 1,
+      name: 'Kullanici 1',
+    },
+    {
+      id: 2,
+      name: 'Kullanici 2',
+    },
+    {
+      id: 3,
+      name: 'Kullanici 3',
+    },
+    {
+      id: 4,
+      name: 'Kullanici 4',
+    },
+    {
+      id: 5,
+      name: 'Kullanici 5',
+    },
+    {
+      id: 6,
+      name: 'Kullanici 6',
+    },
+    {
+      id: 7,
+      name: 'Kullanici 7',
+    },
+  ];
   constructor(private userService: UserService) {}
 
   ngOnInit() {
@@ -22,5 +53,10 @@ export class UserListComponent implements OnInit {
         console.log('user listesi alinamadi', error);
       }
     );
+  }
+
+  getRandomUser() {
+    const index = Math.floor(Math.random() * 7);
+    this.users.push(this.randomUser[index]);
   }
 }
